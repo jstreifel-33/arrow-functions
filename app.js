e6e37cd493f1a1ffb80c712f79009f975efc63cb
Joe Streifel <josephstreifel@gmail.com>
2021-10-22 18:21:27 -0700

    completes step 11.

--- app.js
+++ app.js
@@ -176,17 +176,21 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
+//  When joe.scope() is invoked, "this" refers to the Joe object.
 //
 // 2. What is "this" when joe.scopeArrow() is invoked?
+//  When joe.scopeArrow() is invoked, "this" refers to the Window object.
 //
 // 3. Explain why "this" is different when an arrow function is used.
+//  Unlike normal function expressions, arrow functions don't redefine the "this" keyword when invoked. This is intentional and considered
+//  a feature of ES6 arrow functions, as far as I know.
 //
